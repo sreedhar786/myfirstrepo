ae51818cbaf8f1e86e3806fea9f1a4a1341413f7
sreedhar786 <emc.sreedhar@gmail.com>
2016-12-24 06:23:55 -0800

    four

--- chef-repo/cookbooks/sreedhar/recipes/default.rb
+++ chef-repo/cookbooks/sreedhar/recipes/default.rb
@@ -9,10 +9,10 @@
 package 'tomcat7' do
      action :install
 end
-service  'httpd' do
+service  'tomcat7' do
     action [:restart]
 end
-cookbook_file '/var/lib/tomcat8/webapps/benefits.war' do
+cookbook_file '/var/lib/tomcat7/webapps/benefits.war' do
   source 'http://www.oracle.com/webfolder/technetwork/tutorials/obe/fmw/wls/12c/03-DeployApps/files/benefits.war
 '
 mode '0755'
